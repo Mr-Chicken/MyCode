e4d0522cfe44628fe0ff48c9869b34f0c87f3f70
MrChicken <2754395254@qq.com>
2019-03-22 20:06:34 +0800

    add 2.8

---
+++ ch02/ex_2_8.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+// ascll table : http://www.asciitable.com/
+int main() {
+    cout << "\062\115" << endl;
+    cout << "\062\t\115" << endl;
+    return 0;
+}
