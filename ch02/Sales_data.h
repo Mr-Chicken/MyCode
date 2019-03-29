0b940babdaf0e30155442cd5091d3cd98043f00f
MrChicken <2754395254@qq.com>
2019-03-29 20:52:45 +0800

    over ch02

---
+++ ch02/Sales_data.h
@@ -0,0 +1,10 @@
+#include <string>
+#ifndef SALES_DATA_H
+#define SALES_DATA_H
+
+struct Sales_data {
+    std::string isbn;
+    unsigned units_sold;
+    double revenue;
+};
+#endif
---
+++ ch02/ex_2_42.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include "Sales_data.h"
+using namespace std;
+
+bool readSalesData(Sales_data *sd) { // use reference for the better
+    cin >> sd->isbn;
+    cin >> sd->units_sold;
+    cin >> sd->revenue;
+    return true;
+}
+int main() {
+    Sales_data cur, var;
+    cin >> cur.isbn;
+    cin >> cur.units_sold;
+    cin >> cur.revenue;
+    if (cur.isbn != "") {
+	int cnt = 1;
+	unsigned totalPrice = cur.units_sold*cur.revenue;
+	unsigned totalRevenue = 0;
+	while (readSalesData(&var)) {
+	    if (var.isbn == cur.isbn) {
+		cnt++;
+		totalPrice += var.units_sold*var.revenue;
+		cur.units_sold += var.units_sold;
+		cur.revenue += var.revenue;
+		totalRevenue += cur.revenue;
+	    } else {
+		cout << cur.isbn << " " << cur.units_sold << " " << totalPrice << " " << totalRevenue/cnt << endl;
+		cnt = 1;
+		cur.isbn = var.isbn;
+		cur.units_sold = var.units_sold;
+		cur.revenue = var.revenue;
+	    }
+	}
+    } else {
+	cout << "No Sales!?" << endl;
+    }
+    return 0;
+}
