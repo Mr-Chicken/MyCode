ba10c730a35555492d1ea3df4c669b113168a7ec
MrChicken <2754395254@qq.com>
2019-04-17 12:47:14 +0800

    add 4.9

---
+++ ch04/ex_4_28.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Size of char => " << sizeof(char) << endl;
+	cout << "Size of short => " << sizeof(short) << endl;
+	cout << "Size of int => " << sizeof(int) << endl;
+	cout << "Size of long => " << sizeof(long) << endl;
+	cout << "Size of long long => " << sizeof(long long) << endl;
+	cout << "Size of float => " << sizeof(float) << endl;
+	cout << "Size of double => " << sizeof(double) << endl;
+	cout << "Size of long double => " << sizeof(long double) << endl;
+	cout << "Size of unsigned int => " << sizeof(unsigned int) << endl;
+	cout << "Size of unsigned long => " << sizeof(unsigned long) << endl;
+	cout << "Size of unsigned long long => " << sizeof(unsigned long long) << endl;
+	return 0;
+}
