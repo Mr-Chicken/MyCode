e2ca5cae456817d9fb6e0feeddabed14d6e1601f
Mr-Chicken <2754395254@qq.com>
2019-07-25 21:38:53 +0800

    13.1.4

---
+++ ch13/ex_13_17_for_15.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+class numbered {
+public:
+	numbered() {mysn = unique++;}
+	numbered(const numbered &n) {mysn = unique++;}
+	int mysn;
+	static int unique;
+};
+int numbered::unique = 1;
+
+void f(numbered s) {cout << s.mysn << endl;}
+
+int main()
+{
+	numbered a, b = a, c = b;
+	f(a);
+	f(b);
+	f(c);
+	return 0;
+}
