d05c71208614ed8dc7427af968f3cbdf910c09c6
Mr-Chicken <2754395254@qq.com>
2019-08-07 15:02:30 +0800

    15.2.1

---
+++ ch15/ex_15_3.h
@@ -0,0 +1,27 @@
+#ifndef EX_15_3_H
+#define EX_15_3_H
+#include <iostream>
+using std::string;
+
+class Quote {
+public:
+	Quote() = default;
+	Quote(const string &book, double sales_price):
+		bookNo(book), price(sales_price) {}
+	string isbn() const {return bookNo;}
+	virtual double net_price(std::size_t n) const 
+		{return n * price;}
+	virtual ~Quote() = default;
+private:
+	string bookNo;
+protected:
+	double price = 0.0;
+};
+
+inline double print_total(std::ostream &os, const Quote &item, std::size_t n) {
+	double ret = item.net_price(n);
+	os << "ISBN: " << item.isbn() << " # sold: " << n << " total due: " << ret << endl;
+	return ret;
+};
+
+#endif
