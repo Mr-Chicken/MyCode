5412be214aa2a81f0e736e90870b2cc90c518224
MrChicken <2754395254@qq.com>
2019-08-19 18:56:53 +0800

    16.3

---
+++ ch16/ex_16_48.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <sstream>
+using std::string;
+using std::ostringstream;
+template <typename T>
+string debug_rep(const T &t) {
+	ostringstream ret;
+	ret << t;
+	return ret.str();
+}
+template <typename T>
+string debug_rep(T *p) {
+	ostringstream ret;
+	ret << "pointer: " << p;
+	if (p)
+		ret << " " << debug_rep(*p);
+	else
+		ret << " " << "null pointer";
+	return ret.str();
+}
+string debug_rep(const string &s) {
+	return '"' + s + '"';
+}
+string debug_rep(char *p) {
+	return debug_rep(string(p));
+}
+string debug_rep(const char *p) {
+	return debug_rep(string(p));
+}
